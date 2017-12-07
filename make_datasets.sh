0f1f10ff152a3919e64231a8dce097ea45a50efd
CherylZR <34064993+CherylZR@users.noreply.github.com>
2017-12-07 12:47:13 +0800

    Create make_datasets.sh

---
+++ make_datasets.sh
@@ -0,0 +1,1 @@
+python make_datasets.py --data_path /data/rui.wu/zrui/data_collection/dogcat/train --path_A /data/rui.wu/zrui/data_collection/dogcat/cat --path_B /data/rui.wu/zrui/data_collection/dogcat/cat
