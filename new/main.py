f2ada699bfb44075edcbe7d9885fb14557a9b9ec
CherylZR <34064993+CherylZR@users.noreply.github.com>
2017-12-10 17:36:17 +0800

    Create main.py

---
+++ new/main.py
@@ -0,0 +1,65 @@
+import os
+import argparse
+from torch.backends import cudnn
+from data_loader import get_loader
+from solver import Solver
+
+def main(args):
+    # for fast training
+    cudnn.benchmark = True
+
+    # create directories if not exist
+    if not os.path.exists(args.log_path):
+        os.makedirs(args.log_path)
+    if not os.path.exists(args.model_path):
+        os.makedirs(args.model_path)
+    if not os.path.exists(args.sample_path):
+        os.makedirs(args.sample_path)
+    if not os.path.exists(args.result_path):
+        os.makedirs(args.result_path)
+
+    # data loader
+    data_loader = get_loader()
+
+    # solver
+    solver = Solver(data_loader, args)
+
+    # mode selection
+    if args.mode == 'train':
+        solver.train()
+    elif args.mode == 'test':
+        solver.test()
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    # hyper-parameters
+    parser.add_argument('--lr_g', default=1e-4, type=float)
+    parser.add_argument('--lr_d', default=1e-4, type=float)
+    parser.add_argument('--beta1', default=0.5, type=float)
+    parser.add_argument('--beta2', default=0.999, type=float)
+
+    # training settings
+    parser.add_argument('--mode', default='train', type=str)
+    parser.add_argument('--gpu', default=0, type=str)
+    parser.add_argument('--batch_size', default=1, type=str)
+    parser.add_argument('--serial_batches', action='store_true')
+    parser.add_argument('--scale_size', default=400, type=str)
+    parser.add_argument('--G', default='unet', type=str)
+    parser.add_argument('--D', default='patchgan', type=str)
+    parser.add_argument('--norm_type', default='instance', type=str)
+
+    # test settings
+    parser.add_argument('--test_model', default='20_1000', type=str)
+
+    # paths
+    parser.add_argument('--data_path', default='./datasets/cloud', type=str)
+    parser.add_argument('--log_path', default='./exp/<time>/log', type=str)
+    parser.add_argument('--model_path', default='./exp/<time>/model', type=str)
+    parser.add_argument('--sample_path', default='./exp/<time>/sample', type=str)
+    parser.add_argument('--result_path', default='./exp/<time>/', type=str)
+
+
+    args = parser.parse_args()
+    print(args)
+    main(args)
